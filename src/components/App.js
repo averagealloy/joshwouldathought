9ad08b09eee4f5a10442cfdb9416ed37ff0a25a6
averagealloy <m.mctighe10@gmail.com>
2020-08-12 11:16:33 -0400

    returning a header the code is from another poj mit just a proof of concept for now

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import Header from './Header'
+
+// const App = () => {
+//     return(
+//         <div>
+//             <h1>what this website needs</h1> 
+            
+//             <h2>nav bar</h2>
+//             <h3>fotter with email and P.O. box</h3>
+//             <p> home/about/transport services/packing services/Contact page</p>
+            
+//         </div>
+                
+//         ) 
+// }
+
 
 const App = () => {
     return(
-        <div>
-            <h1>what this website needs</h1> 
-            
-            <h2>nav bar</h2>
-            <h3>fotter with email and P.O. box</h3>
-            <p> home/about/transport services/packing services/Contact page</p>
-            
-        </div>
-                
-        ) 
+        <Header/>
+    )
 }
+
 
 export default App
