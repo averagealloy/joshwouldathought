34d4436318516bfb844b75522ba0e6a9272b2432
averagealloy <m.mctighe10@gmail.com>
2020-08-12 11:28:31 -0400

    footer is messing upo but this is good progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import Header from './Header'
-
-// const App = () => {
-//     return(
-//         <div>
-//             <h1>what this website needs</h1> 
-            
-//             <h2>nav bar</h2>
-//             <h3>fotter with email and P.O. box</h3>
-//             <p> home/about/transport services/packing services/Contact page</p>
-            
-//         </div>
-                
-//         ) 
-// }
-
+import footer from './footer'
 
 const App = () => {
     return(
-        <Header/>
-    )
+        <div>
+            <Header/>
+            <h1>what this website needs</h1> 
+            
+            <h2>nav bar</h2>
+            <h3>fotter with email and P.O. box</h3>
+            <p> home/about/transport services/packing services/Contact page</p>
+
+            <h1><footer/></h1>
+
+        </div>
+        
+        
+                
+        ) 
 }
+
+
+// const App = () => {
+//     return(
+//         <Header/>
+
+        
+//     )
+// }
 
 
 export default App
---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+
+// class footer extends React.Component {
+
+
+// export default footer;
