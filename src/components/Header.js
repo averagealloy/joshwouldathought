b0a7aa81a343ef497f0bd36ae7598561ba291bd0
averagealloy <m.mctighe10@gmail.com>
2020-08-21 15:59:44 -0400

    trying to add a phoito to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 const Header = () => {
-     return <div>Header</div>
+     return <MainContainer>Header</MainContainer>
 };
 
 export default Header
+
+//MAIN CONT 
+
+const MainContainer = styled.header`
+background: url(https://twitter.com/charlieINTEL/status/1296506504607080448/photo/1)no-repeat center/cover;
+height: 25rem
+`;
