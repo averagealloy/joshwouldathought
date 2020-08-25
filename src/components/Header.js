35e2b0f0e5471d117ae74c73cf42cfd6705f10c5
averagealloy <m.mctighe10@gmail.com>
2020-08-25 10:53:17 -0400

    shaq's head is now my header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
      return <MainContainer>Header</MainContainer>
 };
 
-export default Header
+export default Header;
 
 //MAIN CONT 
 
 const MainContainer = styled.header`
-background: url(https://twitter.com/charlieINTEL/status/1296506504607080448/photo/1)no-repeat center/cover;
+background: url(../../images/shaq.jpg)no-repeat center/cover;
 height: 25rem
 `;
