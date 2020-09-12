c18e81a94408bb062b9656608eedac2711874005
averagealloy <m.mctighe10@gmail.com>
2020-09-12 12:34:11 -0400

    changed the logo size in the nav bar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,25 +6,25 @@ export const Navbar = () => {
     return (
         <NavbarContainer>
             <nav className="navbar navbar-expand-lg navbar-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <img src={logo} alt="logo"/>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+            <a className="navbar-brand" href="#">Navbar</a>
+            <img  style={{width: '50px'}} src={logo} alt="logo"/>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
 
-    </ul>
+              </ul>
    
-  </div>
-</nav>
+            </div>
+            </nav>
         </NavbarContainer>
     )
 }
